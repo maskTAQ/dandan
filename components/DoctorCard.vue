68e01279ccff2b1e07f8d474390c740535f54e23
chengqi7 <qi.cheng@gziweb.com>
2022-12-11 12:30:03 +0800

    医生列表

--- components/DoctorCard.vue
+++ components/DoctorCard.vue
@@ -1,5 +1,25 @@
 <template>
-  <div v-if="data" @click="handleClick" :class="['doctor-card', { shadow }]">
+  <div v-if="data" class="doctor-card">
+    <div class="doctor-head flex-row main-between center">
+      <CoverImage :url="data.exPhoto" class="exPhoto" />
+      <div class="info">
+        <p class="title">{{ data.exName }}</p>
+        <p class="exRoom">{{ data.exRoom }}</p>
+        <div>
+          <span class="tag">{{ data.tag }}公立三甲</span>
+          <span class="exLevel">{{ data.exLevel }}</span>
+        </div>
+      </div>
+    </div>
+    <div class="doctor-footer flex-row main-between center">
+      <div>
+        <div class="exRoom ellipsis">{{ data.exRoom }}</div>
+        <div class="intro ellipsis">{{ data.intro }}111111111111111111111111111111111111111111111111</div>
+      </div>
+      <div class="btn">医生主页</div>
+    </div>
+  </div>
+  <!-- <div v-if="data" @click="handleClick" :class="['doctor-card', { shadow }]">
     <div class="main flex-row center">
       <van-icon
         class="portrait align"
@@ -43,11 +63,12 @@
     </div>
 
     <p v-if="price" class="price">￥{{ price }}</p>
-  </div>
+  </div> -->
 </template>
 <script>
+import CoverImage from "@/components/CoverImage.vue"
 import { icons } from "@/assets";
-import { router,goKf } from "@/utils";
+import { router, goKf } from "@/utils";
 export default {
   name: "DoctorCard",
   props: {
@@ -85,6 +106,10 @@ export default {
     badge: [String, Number],
     click: Function,
   },
+  components: {
+    CoverImage
+
+  },
   data() {
     return {
       icons,
@@ -117,138 +142,201 @@ export default {
 </script>
 <style lang="scss">
 @import "../assets/theme.scss";
+
 .doctor-card {
-  /* margin-top: 0.28rem; */
-  position: relative;
-  padding: 0 0.16rem;
-  background: #fff;
+  background: rgb(255, 255, 255);
+  border: 1px solid rgb(232, 243, 241);
+  border-radius: 16px;
+  padding: 0.17rem;
 
-  &.shadow {
-    box-shadow: 0px 0.04rem 0.12rem 0px rgba(76, 135, 249, 0.05);
-    border-radius: 0.08rem;
-  }
-  .my-docyor-tag {
-    position: absolute;
-    width: 0.66rem;
-    height: 0.2rem;
-    padding-right: 0.05rem;
-    left: 0;
-    top: -0.05rem;
-    font-size: 0.1rem;
-    color: #fff;
-    background: url("../assets/img/my-doctor.png") no-repeat center center /
-      100% 100%;
-  }
-  .main {
-    height: 0.9rem;
-    .portrait {
-      /* display: flex; */
-      /* width: 0.48rem;
-      height: 0.48rem;
-      border-radius: 50%; */
-      /* border: 1px solid red; */
-      img {
-        display: block;
-        width: 0.48rem;
-        height: 0.48rem;
-        border-radius: 50%;
-      }
+  .doctor-head {
+    .exPhoto {
+      height: 0.9rem;
+      width: 0.9rem;
+      margin-right: 0.10rem;
     }
+
+    .title {
+      font-size: 0.15rem;
+    }
+
+    .exRoom {
+      font-size: 0.13rem;
+      color: #989898;
+      line-height: 30px;
+      margin-bottom: 0.10rem;
+      border-bottom: 1px solid #e0e0e0;
+    }
+
+    .tag {
+      background: #eaf3f1;
+      color: #48988e;
+      border-radius: 0.05rem;
+      padding: 0.05rem;
+      font-weight: 600;
+    }
+
     .info {
-      width: 0;
-      flex: 1;
-      margin: 0 0.14rem;
-      .top {
-        color: $color4;
-        font-weight: 500;
-        align-items: flex-end;
-        .name {
-          margin-right: 0.08rem;
-          font-size: 0.18rem;
-        }
-        .title-box {
-          width: 100%;
-          .title {
-            width: 0;
-            flex: 1;
-            font-size: 0.12rem;
-          }
-        }
+      flex: 1
+    }
+  }
 
-        .time {
-          font-size: 0.11rem;
-          color: #999999;
-        }
-      }
-      .tag {
-        margin-right: 0.04rem;
-        height: 0.16rem;
-        padding: 0 0.04rem;
-        line-height: 0.16rem;
-        font-size: 0.09rem;
-        color: #fff;
-        background: $color1;
-        border-radius: 0.03rem;
-      }
-      .hospital {
-        margin-top: 0.08rem;
-        font-size: 0.12rem;
-        color: $color6;
-      }
+  .doctor-footer {
+    margin-top:0.10rem;
+    .btn {
+      /* Rectangle 515 */
+      width: 1.12rem;
+      height: 0.34rem;
+      background: rgb(0, 188, 197);
+      border-radius: 4px;
+      color: #fff;
+      font-size: 0.12rem;
+      text-align: center;
+      line-height: 0.34rem;
     }
-    .intro {
-      margin-top: 0.02rem;
-      font-size: 0.11rem;
-      color: $color5;
+    .exRoom{
+      color: rgb(0, 188, 197);
+      width: 100px;
     }
-    .ask {
-      img {
-        width: 0.22rem;
-      }
-      .label {
-        margin-top: 0.07rem;
-        font-size: 0.12rem;
-        color: $color5;
-      }
+    .intro{
+      color: #989898;
+      width: 150px;
     }
-  }
-  .intro-box {
-    &.unfold {
-      p {
-        height: 0.2rem;
-        overflow: hidden;
-      }
-    }
-    p {
-      width: 100%;
-      line-height: 0.2rem;
-    }
-    .toggle {
-      margin: 0.08rem 0;
-      width: 0.12rem;
-    }
-  }
-  .reply {
-    padding: 0.1rem 0;
-    border-top: 1px solid $border;
-    font-size: 0.11rem;
-    .label {
-      margin-right: 4px;
-      color: $color1;
-    }
-    .value {
-      width: 0;
-      flex: 1;
-      color: $color5;
-    }
-  }
-  .price {
-    position: absolute;
-    top: 0.2rem;
-    right: 0;
-    font-size: 0.14rem;
-    font-weight: bold;
-    color: $color1;
   }
 }
+
+// .doctor-card {
+//   /* margin-top: 0.28rem; */
+//   position: relative;
+//   padding: 0 0.16rem;
+//   background: #fff;
+
+//   &.shadow {
+//     box-shadow: 0px 0.04rem 0.12rem 0px rgba(76, 135, 249, 0.05);
+//     border-radius: 0.08rem;
+//   }
+//   .my-docyor-tag {
+//     position: absolute;
+//     width: 0.66rem;
+//     height: 0.2rem;
+//     padding-right: 0.05rem;
+//     left: 0;
+//     top: -0.05rem;
+//     font-size: 0.1rem;
+//     color: #fff;
+//     background: url("../assets/img/my-doctor.png") no-repeat center center /
+//       100% 100%;
+//   }
+//   .main {
+//     height: 0.9rem;
+//     .portrait {
+//       /* display: flex; */
+//       /* width: 0.48rem;
+//       height: 0.48rem;
+//       border-radius: 50%; */
+//       /* border: 1px solid red; */
+//       img {
+//         display: block;
+//         width: 0.48rem;
+//         height: 0.48rem;
+//         border-radius: 50%;
+//       }
+//     }
+//     .info {
+//       width: 0;
+//       flex: 1;
+//       margin: 0 0.14rem;
+//       .top {
+//         color: $color4;
+//         font-weight: 500;
+//         align-items: flex-end;
+//         .name {
+//           margin-right: 0.08rem;
+//           font-size: 0.18rem;
+//         }
+//         .title-box {
+//           width: 100%;
+//           .title {
+//             width: 0;
+//             flex: 1;
+//             font-size: 0.12rem;
+//           }
+//         }
+
+//         .time {
+//           font-size: 0.11rem;
+//           color: #999999;
+//         }
+//       }
+//       .tag {
+//         margin-right: 0.04rem;
+//         height: 0.16rem;
+//         padding: 0 0.04rem;
+//         line-height: 0.16rem;
+//         font-size: 0.09rem;
+//         color: #fff;
+//         background: $color1;
+//         border-radius: 0.03rem;
+//       }
+//       .hospital {
+//         margin-top: 0.08rem;
+//         font-size: 0.12rem;
+//         color: $color6;
+//       }
+//     }
+//     .intro {
+//       margin-top: 0.02rem;
+//       font-size: 0.11rem;
+//       color: $color5;
+//     }
+//     .ask {
+//       img {
+//         width: 0.22rem;
+//       }
+//       .label {
+//         margin-top: 0.07rem;
+//         font-size: 0.12rem;
+//         color: $color5;
+//       }
+//     }
+//   }
+//   .intro-box {
+//     &.unfold {
+//       p {
+//         height: 0.2rem;
+//         overflow: hidden;
+//       }
+//     }
+//     p {
+//       width: 100%;
+//       line-height: 0.2rem;
+//     }
+//     .toggle {
+//       margin: 0.08rem 0;
+//       width: 0.12rem;
+//     }
+//   }
+//   .reply {
+//     padding: 0.1rem 0;
+//     border-top: 1px solid $border;
+//     font-size: 0.11rem;
+//     .label {
+//       margin-right: 4px;
+//       color: $color1;
+//     }
+//     .value {
+//       width: 0;
+//       flex: 1;
+//       color: $color5;
+//     }
+//   }
+//   .price {
+//     position: absolute;
+//     top: 0.2rem;
+//     right: 0;
+//     font-size: 0.14rem;
+//     font-weight: bold;
+//     color: $color1;
+//   }
+// }
 </style>
